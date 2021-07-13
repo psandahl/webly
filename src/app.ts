b12baea13f7cdafedadf33930b84b2286735b868
Patrik Sandahl <patrik.sandahl@gmail.com>
2021-07-13 12:48:47 +0200

    Minor refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,13 @@
 import { getWindowSize } from './dom';
 
-function onLoad() {
+window.onload = () => {
     const [w, h] = getWindowSize();
     const msg = `Window loaded with size width: ${w} and height: ${h}`;
     document.body.innerHTML = msg;
 }
 
-function onResize() {
+window.onresize = () => {
     const [w, h] = getWindowSize();
     const msg = `Window resized with size width: ${w} and height: ${h}`;
     document.body.innerHTML = msg;
 }
-
-window.onload = onLoad;
-window.onresize = onResize;
