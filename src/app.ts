58d210d077b6f9088a31026fbf5d03b1d0068e0f
Patrik Sandahl <patrik.sandahl@gmail.com>
2021-07-13 13:17:09 +0200

    Refactoring with some more fun DOM-stuff

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,23 @@ import { getWindowSize } from './dom';
 window.onload = () => {
     const [w, h] = getWindowSize();
     const msg = `Window loaded with size width: ${w} and height: ${h}`;
-    document.body.innerHTML = msg;
+
+    const p = document.createElement('p');
+    p.textContent = msg;
+    document.body.appendChild(p);    
 }
 
 window.onresize = () => {
     const [w, h] = getWindowSize();
     const msg = `Window resized with size width: ${w} and height: ${h}`;
-    document.body.innerHTML = msg;
+
+    let p = document.getElementById('resizeinfo');
+    if (p != null) {
+        p.textContent = msg;
+    } else {
+        p = document.createElement('p');
+        p.id = "resizeinfo";
+        p.textContent = msg;
+        document.body.appendChild(p);
+    }    
 }
