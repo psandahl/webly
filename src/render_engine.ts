66ac83257f35052c8b18e3f64e4da9405acd66db
Patrik Sandahl <patrik.sandahl@gmail.com>
2021-07-15 10:22:57 +0200

    Simplest triangle

--- src/render_engine.ts
+++ src/render_engine.ts
@@ -5,7 +5,7 @@ export class RenderEngine {
 
     private _canvas: HTMLCanvasElement;
     private _gl: WebGL2RenderingContext;
-    private _program0: WebGLProgram;
+    private _program0: WebGLProgram = null!;
     
     /**
      * Create a RenderEngine.
@@ -18,22 +18,33 @@ export class RenderEngine {
             throw new Error("Unable to create WebGL2");
         }
 
-        const [succ, program] = compileShaderProgram(this._gl, this._vs0, this._fs0);
-        if (!succ) {
-            throw new Error("Failed to compile shader program");
-        }
-
-        this._program0 = program;
-
         document.body.appendChild(this._canvas);        
     }
 
     /**
      * Start the engine.
      */
-    public start() {        
+    public start() {
 
-        this._gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        // Compile program.
+        const [succ, program] = compileShaderProgram(this._gl, this._vs0, this._fs0);
+        if (!succ) {
+            throw new Error("Failed to compile shader program");
+        }
+        
+        this._program0 = program;
+
+        // Create and populate position buffer.
+        this._posBuf0 = this._gl.createBuffer()!;
+        this._gl.bindBuffer(this._gl.ARRAY_BUFFER, this._posBuf0);
+        this._gl.bufferData(this._gl.ARRAY_BUFFER, new Float32Array(this._pos0), this._gl.STATIC_DRAW);
+
+        // Setup vao.
+        const positionAttributeLocation = this._gl.getAttribLocation(this._program0, "a_position");
+        this._vao0 = this._gl.createVertexArray()!;
+        this._gl.bindVertexArray(this._vao0);
+        this._gl.enableVertexAttribArray(positionAttributeLocation);
+        this._gl.vertexAttribPointer(positionAttributeLocation, 2, this._gl.FLOAT, false, 0, 0);
 
         this.loop();
     }
@@ -52,19 +63,35 @@ export class RenderEngine {
 
     private loop() {
         
+        const [width, height] = getWindowSize();
+
+        this._gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        this._gl.viewport(0, 0, width, height);
         this._gl.clear(this._gl.COLOR_BUFFER_BIT);
+
         this._gl.useProgram(this._program0);
-        
+        this._gl.bindVertexArray(this._vao0);
+        this._gl.drawArrays(this._gl.TRIANGLES, 0, 3);
+
         requestAnimationFrame(this.loop.bind(this));
     }
+
+    private _pos0 = [
+        0.0, 0.25,
+        -0.1, -0.1,
+        0.4, -0.4
+    ];
+
+    private _posBuf0: WebGLBuffer = null!;
+    private _vao0: WebGLVertexArrayObject = null!;
 
     private _vs0 = 
     `#version 300 es
 
-in vec3 a_position;
+in vec4 a_position;
 
 void main() {
-    gl_Position = vec4(a_position, 1);
+    gl_Position = a_position;
 }
 `;
 
