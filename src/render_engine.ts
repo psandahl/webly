f113827b288be1231e5024e56330fe699261dc38
Patrik Sandahl <patrik.sandahl@gmail.com>
2021-07-15 14:42:42 +0200

    Added colors

--- src/render_engine.ts
+++ src/render_engine.ts
@@ -18,7 +18,9 @@ export class RenderEngine {
             throw new Error("Unable to create WebGL2");
         }
 
-        document.body.appendChild(this._canvas);        
+        document.body.appendChild(this._canvas);
+
+        console.log(`Max number of vertex attributes: ${this._gl.getParameter(this._gl.MAX_VERTEX_ATTRIBS)}`);
     }
 
     /**
@@ -35,16 +37,26 @@ export class RenderEngine {
         this._program0 = program;
 
         // Create and populate position buffer.
-        this._posBuf0 = this._gl.createBuffer()!;
-        this._gl.bindBuffer(this._gl.ARRAY_BUFFER, this._posBuf0);
-        this._gl.bufferData(this._gl.ARRAY_BUFFER, new Float32Array(this._pos0), this._gl.STATIC_DRAW);
+        this._attrBuf0 = this._gl.createBuffer()!;
+        this._gl.bindBuffer(this._gl.ARRAY_BUFFER, this._attrBuf0);
+        this._gl.bufferData(this._gl.ARRAY_BUFFER, new Float32Array(this._attr0), this._gl.STATIC_DRAW);
 
         // Setup vao.
         const positionAttributeLocation = this._gl.getAttribLocation(this._program0, "a_position");
+        console.log(`a_position num: ${positionAttributeLocation}`);
+        const colorAttributeLocation = this._gl.getAttribLocation(this._program0, "a_color");
+        console.log(`a_color num: ${colorAttributeLocation}`);
+
         this._vao0 = this._gl.createVertexArray()!;
         this._gl.bindVertexArray(this._vao0);
+
+        // Position.
         this._gl.enableVertexAttribArray(positionAttributeLocation);
-        this._gl.vertexAttribPointer(positionAttributeLocation, 2, this._gl.FLOAT, false, 0, 0);
+        this._gl.vertexAttribPointer(positionAttributeLocation, 2, this._gl.FLOAT, false, 5 * 4, 0);
+
+        // Color.
+        this._gl.enableVertexAttribArray(colorAttributeLocation);
+        this._gl.vertexAttribPointer(colorAttributeLocation, 3, this._gl.FLOAT, false, 5 * 4, 2 * 4);
 
         this.loop();
     }
@@ -76,21 +88,25 @@ export class RenderEngine {
         requestAnimationFrame(this.loop.bind(this));
     }
 
-    private _pos0 = [
-        0.0, 0.25,
-        -0.1, -0.1,
-        0.4, -0.4
+    private _attr0 = [
+        0.0, 0.25, 1.0, 0.0, 0.0,
+        -0.1, -0.1, 0.0, 1.0, 0.0,
+        0.4, -0.4, 0.0, 0.0, 1.0
     ];
 
-    private _posBuf0: WebGLBuffer = null!;
+    private _attrBuf0: WebGLBuffer = null!;
     private _vao0: WebGLVertexArrayObject = null!;
 
     private _vs0 = 
     `#version 300 es
 
 in vec4 a_position;
+in vec3 a_color;
+
+out vec3 v_color;
 
 void main() {
+    v_color = a_color;
     gl_Position = a_position;
 }
 `;
@@ -99,11 +115,12 @@ void main() {
     `#version 300 es
 
 precision highp float;
-    
+  
+in vec3 v_color;
 out vec4 color;
 
 void main() {
-    color = vec4(1, 0, 0.5, 1);
+    color = vec4(v_color, 1);
 }
 `;
 };
